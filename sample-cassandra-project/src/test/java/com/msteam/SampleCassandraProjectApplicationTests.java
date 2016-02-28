613604adff0d745dec6eea321c01f04ecb36445d
szogi <szogibalu@gmail.com>
2016-02-28 07:58:56 +0100

    MS-18 Fixed SampleCassandraProjectApplicationTests test

--- sample-cassandra-project/src/test/java/com/msteam/SampleCassandraProjectApplicationTests.java
+++ sample-cassandra-project/src/test/java/com/msteam/SampleCassandraProjectApplicationTests.java
@@ -1,16 +1,24 @@
 package com.msteam;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SampleCassandraProjectApplication.class)
 public class SampleCassandraProjectApplicationTests {
 
+	@Autowired
+	ApplicationContext context;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull(context);
 	}
 
 }
