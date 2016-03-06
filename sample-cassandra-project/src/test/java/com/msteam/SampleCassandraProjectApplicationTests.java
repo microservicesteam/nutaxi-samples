d4b9d518e4210a842eda2f7e43ce38a41dec57a9
szogi <szogibalu@gmail.com>
2016-03-06 20:20:22 +0100

    MS-18 Fixed broken build

--- sample-cassandra-project/src/test/java/com/msteam/SampleCassandraProjectApplicationTests.java
+++ sample-cassandra-project/src/test/java/com/msteam/SampleCassandraProjectApplicationTests.java
@@ -7,10 +7,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SampleCassandraProjectApplication.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class SampleCassandraProjectApplicationTests {
 
 	@Autowired
