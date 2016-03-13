abb8273bf403561373d28fd711b5e0106c82ac14
szogi <szogibalu@gmail.com>
2016-03-13 18:38:12 +0100

    MS-18 Added Kubernetes setup

--- sample-cassandra-kubernetes-project/README.md
+++ sample-cassandra-kubernetes-project/README.md
@@ -4,4 +4,9 @@
 
  - Install [Docker](https://docs.docker.com/engine/installation/)
  - Setup [Kubernetes](http://kubernetes.io/docs/getting-started-guides/docker/)
+  
+ ```sh
+ ./configuration/kubernetes_install.sh
+``` 
+ 
  - Setup [Cassandra cluster](https://github.com/kubernetes/kubernetes/tree/release-1.2/examples/cassandra)
---
+++ sample-cassandra-kubernetes-project/configuration/kubernetes_install.sh
@@ -0,0 +1,20 @@
+docker run \
+    --volume=/:/rootfs:ro \
+    --volume=/sys:/sys:ro \
+    --volume=/var/lib/docker/:/var/lib/docker:rw \
+    --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
+    --volume=/var/run:/var/run:rw \
+    --net=host \
+    --pid=host \
+    --privileged=true \
+    -d \
+    gcr.io/google_containers/hyperkube-amd64:v1.2.0-alpha.8 \
+    /hyperkube kubelet \
+        --containerized \
+        --hostname-override="127.0.0.1" \
+        --address="0.0.0.0" \
+        --api-servers=http://localhost:8080 \
+        --config=/etc/kubernetes/manifests \
+        --cluster-dns=10.0.0.10 \
+        --cluster-domain=cluster.local \
+        --allow-privileged=true --v=2
