f86255c2b4319dd9e027d213d478ec43020e4fbc
Andor Kelemen <andor.kelemen@gmail.com>
2016-03-31 20:25:01 +0200

    M-24 Create Admin portal and register Hello World on it

---
+++ sample-spring-boot-app/02_1_hello-world-actuator-admin/src/main/java/com/msteam/helloworld/SpringBootAdminApplication.java
@@ -0,0 +1,13 @@
+package com.msteam.helloworld;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminApplication {
+        public static void main(String[] args){
+            SpringApplication.run(SpringBootAdminApplication.class, args);
+        }
+}
